ff89270d3996bcb431537de9d89b414a81fb8583
songchongjun <2220793775@qq.com>
2016-03-12 02:10:57 +0800

    清理作业根目录文件

--- start.js
+++ start.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var app = express();
-var iconv = require('iconv-lite');
-var path = require('path');
-var os = require('os')
+
 
 var root = process.argv[2];
 if (!root) {
@@ -23,27 +21,11 @@ var server = app.listen(3000, function () {
 
 function typescriptCompiler(req, res, next) {
     var spawn = require('child_process').spawn;
-    
-    var tsc_path;
-    if (os.platform() == 'win32'){
-        tsc_path = path.join('node_modules','.bin','tsc.cmd');
-    }
-    else{
-        tsc_path =  path.join('node_modules','.bin','tsc');
-    }
-    var tsc = spawn(tsc_path, ['-p',root]);
+    var tsc = spawn('tsc', ['-p',root]);
     var errorMessage = "";
     // 捕获标准输出并将其打印到控制台
     tsc.stdout.on('data', function (data) {
         errorMessage += data;
-    });
-      // 捕获标准输出并将其打印到控制台
-    tsc.stderr.on('data', function (data) {
-        
-          var buffer = new Buffer(data);
-          var str = iconv.decode(buffer, 'gbk');;
-          console.log (str)
-        errorMessage += str;
     });
 
     // 注册子进程关闭事件
