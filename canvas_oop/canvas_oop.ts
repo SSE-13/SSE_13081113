df11fd144d400972549229cdf5e9b99d6fae742c
songchongjun <2220793775@qq.com>
2016-03-12 13:18:34 +0800

    绘制

--- canvas_oop/canvas_oop.ts
+++ canvas_oop/canvas_oop.ts
@@ -59,11 +59,14 @@ class Rect extends DisplayObject {
 }
 
 class TextField extends DisplayObject {
+    font = "20px Arial";
+    color = '#000000';
+    filltext = 'HelloWorld';
 
     render(context: CanvasRenderingContext2D) {
-        context.font = "20px Arial";
-        context.fillStyle = '#000000';
-        context.fillText('HelloWorld', 0, 20);
+        context.font = this.font;
+        context.fillStyle = this.color;
+        context.fillText(this.filltext, 0, 20);
     }
 }
 
@@ -104,29 +107,58 @@ var context = canvas.getContext("2d");
 
 
 var rect = new Rect();
-rect.width = 200;
-rect.height = 100;
-rect.color = '#00FF00'
-
+rect.width = 355;
+rect.height = 362;
+rect.color = '#F0F0F0'
 
 var rect2 = new Rect();
-rect2.width = 300;
-rect2.height = 50;
-rect2.x = 200;
-rect2.y = 200;
-rect2.rotation = Math.PI / 8;
-rect2.color = '#00FFFF'
+rect2.width = 60;
+rect2.height = 27;
+rect2.x = 86;
+rect2.y = 324;
+rect2.color = '#0000FF'
+
+var rect3 = new Rect();
+rect3.width = 60;
+rect3.height = 27;
+rect3.x = 205;
+rect3.y = 324;
+rect3.color = '#0000FF'
 
 var text = new TextField();
-text.x = 10;
+text.x = 110;
+text.y = 327;
+text.font = "20px Arial";
+text.color = '#FFFFFF';
+text.filltext = '0';
+
+var text2 = new TextField();
+text2.x = 224;
+text2.y = 327;
+text2.font = "20px Arial";
+text2.color = '#FFFFFF';
+text2.filltext = '10';
+
 
 var bitmap = new Bitmap();
-bitmap.source = 'wander-icon.jpg';
+bitmap.source = 'fangge.jpg';
+bitmap.x=45;
+bitmap.y=48;
+
+var bitmap2 = new Bitmap();
+bitmap2.source = 'icon1.jpg';
+bitmap2.x=33;
+bitmap2.y=317;
+
+var bitmap3 = new Bitmap();
+bitmap3.source = 'icon2.jpg';
+bitmap3.x=282;
+bitmap3.y=317;
 
 //渲染队列
-var renderQueue = [rect, rect2, text,bitmap];
+var renderQueue = [rect, rect2,rect3, text,text2,bitmap,bitmap2,bitmap3];
 //资源加载列表
-var imageList = ['wander-icon.jpg'];
+var imageList = ['fangge.jpg','icon1.jpg','icon2.jpg'];
 
 //先加载资源，加载成功之后执行渲染队列
 loadResource(imageList, function() {
